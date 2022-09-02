3846549d9e1791b704693effd0fd946aa7ed6769
mosherif21 <95943033+mosherif21@users.noreply.github.com>
2022-09-02 16:11:27 +0200

    Ridetogo-APP

--- app/src/main/java/com/example/ridetogo/password_change_activity.java
+++ app/src/main/java/com/example/ridetogo/password_change_activity.java
@@ -17,36 +17,43 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class password_change_activity extends AppCompatActivity {
-    private FirebaseUser user;
 
+    //ui vars
     private Button btn_password_save;
     private EditText old_password;
     private EditText new_password;
     private TextView password_error;
 
+    //firebase vars
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password_change);
         getSupportActionBar().hide();
+
+        //link ui vars
         btn_password_save = findViewById(R.id.btn_save_newPassword);
         old_password = findViewById(R.id.old_password_input);
         new_password = findViewById(R.id.new_password_input);
         password_error = findViewById(R.id.txt_password_change_error);
 
-
+        //click listener for new password save
         btn_password_save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //if new password is less than 8 characters display error textview
                 if (new_password.getText().toString().trim().length() < 8) {
                     password_error.setText("New password can't be less than 8 characters");
                     password_error.setVisibility(View.VISIBLE);
                 } else {
+                    //if old password is less than 8 characters than it must be incorrect
                     if (old_password.getText().toString().trim().length() < 8) {
                         password_error.setText("old password is incorrect");
                         password_error.setVisibility(View.VISIBLE);
                     } else {
+                        //re-authenticate with old password and user email to check if old password entered is incorrect
                         password_error.setVisibility(View.INVISIBLE);
                         user = FirebaseAuth.getInstance().getCurrentUser();
                         final String email = user.getEmail();
@@ -55,8 +62,8 @@ public class password_change_activity extends AppCompatActivity {
                         user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
                             public void onComplete(@NonNull Task<Void> task) {
                                 if (task.isSuccessful()) {
+                                    //if authentication successful then update password with new password
                                     user.updatePassword(new_password.getText().toString().trim()).addOnCompleteListener(new OnCompleteListener<Void>() {
-
                                         public void onComplete(@NonNull Task<Void> task) {
                                             if (!task.isSuccessful()) {
                                                 password_error.setText("An error happened please try again!");
@@ -83,6 +90,7 @@ public class password_change_activity extends AppCompatActivity {
     @Override
     public void finish() {
         super.finish();
+        //override activity finish to animate from up to down
         overridePendingTransition(R.anim.slide_in_down, R.anim.slide_down);
     }
 }
--- app/src/main/java/com/example/ridetogo/perform_login.java
+++ app/src/main/java/com/example/ridetogo/perform_login.java
@@ -29,15 +29,20 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 public class perform_login extends AppCompatActivity {
+
+    //ui vars
     private Button login;
     private EditText pass;
     private EditText email;
     private ProgressBar progressBar;
     private String full_phone_no;
     private String isdriver;
+
+    //firebase vars
     private String child_ref;
     private FirebaseAuth mauth;
 
+    //function to hide keyboard
     private static void hideSoftKeyboard(Activity activity) {
 
         InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
@@ -49,18 +54,30 @@ public class perform_login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perform_login);
         getSupportActionBar().hide();
+
+        //firebase vars initialize
         mauth = FirebaseAuth.getInstance();
+
+        //ui vars link
         login = findViewById(R.id.btn_perform_login);
         pass = findViewById(R.id.password_login_txt);
         email = findViewById(R.id.email_login_txt);
+        progressBar = findViewById(R.id.performlogin_progressbar);
+
+        //make login button not clickable until data is loaded and the user has entered a valid email and password formats
         login.setClickable(false);
+
+        //get user phone number and if he is a driver from previous (login) activity
         full_phone_no = getIntent().getStringExtra("phone_no");
         isdriver = getIntent().getStringExtra("driver");
+
+        //get if user is a rider or driver to check his email
         if (isdriver.equals("yes"))
             child_ref = "Drivers";
         else
             child_ref = "Riders";
-        progressBar = findViewById(R.id.performlogin_progressbar);
+
+        //password edittext change listener to make login button clickable when password is >= 8 characters
         pass.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -75,10 +92,13 @@ public class perform_login extends AppCompatActivity {
                     login.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
+                            //make progress bar visible until login is performed or password or email is incorrect
                             hideSoftKeyboard(perform_login.this);
                             getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
                                     WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
                             progressBar.setVisibility(View.VISIBLE);
+
+                            //check if email is correct
                             FirebaseDatabase database = FirebaseDatabase.getInstance("https://ridetogo-dcf8e-default-rtdb.europe-west1.firebasedatabase.app/");
                             Query checkuser_exists = database.getReference("Users").child(child_ref).orderByChild("Email").equalTo(email.getText().toString().trim());
                             checkuser_exists.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -86,11 +106,12 @@ public class perform_login extends AppCompatActivity {
                                 public void onDataChange(@NonNull DataSnapshot snapshot) {
                                     getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
                                     if (snapshot.exists()) {
-
+                                        //if email is correct sign in using email and password
                                         mauth.signInWithEmailAndPassword(email.getText().toString().trim(), pass.getText().toString().trim()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                                             @Override
                                             public void onComplete(@NonNull Task<AuthResult> task) {
                                                 if (task.isSuccessful()) {
+                                                    //if sign in is successful and isdriver is yes then go to driver activity if it's no go to home activity
                                                     progressBar.setVisibility(View.INVISIBLE);
                                                     Intent intent;
                                                     if (isdriver.equals("yes"))
