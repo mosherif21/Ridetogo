c008785843da7621db5cf49e47df78c67d212221
mosherif21 <95943033+mosherif21@users.noreply.github.com>
2022-09-11 14:40:42 +0200

    Ridetogo-APP

--- app/src/main/java/com/example/ridetogo/RideHistoryPreview.java
+++ app/src/main/java/com/example/ridetogo/RideHistoryPreview.java
@@ -40,17 +40,7 @@ import java.util.Locale;
 import java.util.Map;
 
 public class RideHistoryPreview extends AppCompatActivity implements OnMapReadyCallback, RoutingListener {
-    private static final int[] COLORS = new int[]{R.color.black};
-    private GoogleMap mymap;
-    private SupportMapFragment mapFragment;
-    private String rideId;
-    private String riderId;
-    private String driverId;
-    private LatLng destinationLatlng, pickupLatlng;
-    private String destination_name;
-    private DatabaseReference history_ref;
-    private String userId;
-    //history vars
+    //ui vars
     private TextView txt_from_to;
     private TextView txt_price;
     private TextView txt_date;
@@ -58,18 +48,32 @@ public class RideHistoryPreview extends AppCompatActivity implements OnMapReadyC
     private TextView txt_phone;
     private ImageView userImage;
     private RatingBar ratingBar;
+
+    //google map vars
+    private GoogleMap mymap;
+    private SupportMapFragment mapFragment;
+
+    //ride details vars
+    private String rideId;
+    private String riderId;
+    private String driverId;
+    private LatLng destinationLatlng, pickupLatlng;
+    private String destination_name;
+    private DatabaseReference history_ref;
+    private String userId;
+
     //route drawing vars
     private List<Polyline> polylines;
+    private static final int[] COLORS = new int[]{R.color.black};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ride_history_preview);
         getSupportActionBar().hide();
+
+        //link ui vars
         mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map2);
-        assert mapFragment != null;
-        mapFragment.getMapAsync(this);
-        polylines = new ArrayList<>();
         txt_from_to = findViewById(R.id.txt_ride_from_to_history);
         txt_price = findViewById(R.id.txt_price_ride_history);
         txt_date = findViewById(R.id.txt_ride_date_history_prev);
@@ -77,24 +81,43 @@ public class RideHistoryPreview extends AppCompatActivity implements OnMapReadyC
         txt_phone = findViewById(R.id.txt_driver_phone_history);
         userImage = findViewById(R.id.driver_image_ride_history);
         ratingBar = findViewById(R.id.rating_bar_ride);
+
+        //assign google maps vars
+        assert mapFragment != null;
+        mapFragment.getMapAsync(this);
+
+        //initialize route vars
+        polylines = new ArrayList<>();
+
+        //firebase user initialize
         userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        //get ride id to view from previous activity
         rideId = getIntent().getStringExtra("rideId");
+
+        //get ride reference from database using ride id
         history_ref = FirebaseDatabase.getInstance("https://ridetogo-dcf8e-default-rtdb.europe-west1.firebasedatabase.app/").getReference().child("RidesHistory").child(rideId);
+
+        //call ride history info function
         getRideHistoryInfo();
     }
 
+    //ride info function
     private void getRideHistoryInfo() {
         history_ref.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 if (snapshot.exists()) {
+                    //if ride id data snapshot exists assign ride info to vars
                     for (DataSnapshot history_child : snapshot.getChildren()) {
                         if (history_child.getKey().equals("rider")) {
                             riderId = history_child.getValue().toString();
                         }
                         if (history_child.getKey().equals("driver")) {
                             driverId = history_child.getValue().toString();
+                            //after getting driver id call driver info function using driver id
                             DriverInfo(driverId);
+                            //call rating function
                             ratingFunction();
                         }
                         if (history_child.getKey().equals("timeStamp")) {
@@ -111,12 +134,17 @@ public class RideHistoryPreview extends AppCompatActivity implements OnMapReadyC
                             txt_price.setText(history_child.getValue().toString() + " EGP");
                         }
                         if (history_child.getKey().equals("location")) {
+                            //get pickup location latlng
                             pickupLatlng = new LatLng(Double.parseDouble(history_child.child("from").child("lat").getValue().toString()), Double.parseDouble(history_child.child("from").child("lng").getValue().toString()));
+                            //get destination latlng
                             destinationLatlng = new LatLng(Double.parseDouble(history_child.child("to").child("lat").getValue().toString()), Double.parseDouble(history_child.child("to").child("lng").getValue().toString()));
                             if (pickupLatlng != null && destinationLatlng != null) {
+                                //add pickup and destination markers
                                 mymap.addMarker(new MarkerOptions().position((pickupLatlng)).title("Pickup location"));
                                 mymap.addMarker(new MarkerOptions().position((destinationLatlng)).title(destination_name));
+                                //draw route between pickup and location
                                 getRouteToMarker();
+                                //animate camera to route
                                 LatLngBounds.Builder builder = new LatLngBounds.Builder();
                                 builder.include(pickupLatlng);
                                 builder.include(destinationLatlng);
@@ -138,10 +166,13 @@ public class RideHistoryPreview extends AppCompatActivity implements OnMapReadyC
 
     }
 
+    //rating function
     private void ratingFunction() {
+        //rating bar on rating changed listener
         ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             @Override
             public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                //update ride rating using history reference
                 history_ref.child("rating").setValue(rating);
                 DatabaseReference reference = FirebaseDatabase.getInstance("https://ridetogo-dcf8e-default-rtdb.europe-west1.firebasedatabase.app/").getReference().child("Users").child("Drivers").child(driverId);
                 reference.child("rating").child(rideId).setValue(rating);
@@ -149,12 +180,15 @@ public class RideHistoryPreview extends AppCompatActivity implements OnMapReadyC
         });
     }
 
+    //driver info function
     private void DriverInfo(String aDriverId) {
+        //get driver reference using driver id
         DatabaseReference reference = FirebaseDatabase.getInstance("https://ridetogo-dcf8e-default-rtdb.europe-west1.firebasedatabase.app/").getReference().child("Users").child("Drivers").child(aDriverId);
         reference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 if (snapshot.exists()) {
+                    //get name, phone and profile photo
                     Map<String, Object> datamap = (Map<String, Object>) snapshot.getValue();
                     if (datamap.get("Name") != null) {
                         txt_name.setText(datamap.get("Name").toString());
@@ -178,6 +212,7 @@ public class RideHistoryPreview extends AppCompatActivity implements OnMapReadyC
 
     }
 
+    //get ride date using ride time stamp
     private String getDateOfTimeStamp(Long timestamp) {
         Calendar calendar = Calendar.getInstance(Locale.getDefault());
         calendar.setTimeInMillis(timestamp * 1000);
@@ -191,6 +226,7 @@ public class RideHistoryPreview extends AppCompatActivity implements OnMapReadyC
         googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(RideHistoryPreview.this, R.raw.day_mode));
     }
 
+    //rout draw function already commented in many classes
     private void getRouteToMarker() {
         if (pickupLatlng != null) {
             Routing routing = new Routing.Builder()
@@ -204,7 +240,7 @@ public class RideHistoryPreview extends AppCompatActivity implements OnMapReadyC
         }
     }
 
-
+    //erase route poly lines if needed
     private void eraseRoutePolyLines() {
         for (Polyline pline : polylines) {
             pline.remove();
